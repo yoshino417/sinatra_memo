a7dfb489f30df78cdb9cafa09ceab437f684db47
yoshino417 <toshihiko.yoshino.6s@stu.hosei.ac.jp>
2020-12-25 05:34:46 +0900

    update simultaneous use support

--- app.rb
+++ app.rb
@@ -17,6 +17,18 @@ def write_file(memos)
   end
 end
 
+def allocate_id(memos)
+  memos == [] ? 1 : memos[-1]['id'] + 1
+end
+
+def get_index(memos, memo_id)
+  memo_index = nil
+  memos.each_with_index do |memo, i|
+    memo_index = i if memo['id'] == memo_id
+  end
+  memo_index.nil? ? nil : memo_index
+end
+
 before do
   @memos = read_file
 end
@@ -32,31 +44,39 @@ get '/add' do
 end
 
 post '/add' do
-  @memos << { 'title' => CGI.escapeHTML(params[:title]), 'content' => CGI.escapeHTML(params[:content]) }
+  @memos << { 'title' => CGI.escapeHTML(params[:title]), 'content' => CGI.escapeHTML(params[:content]), 'id' => allocate_id(@memos) }
   write_file(@memos)
   redirect '/'
 end
 
 get '/show/:id' do
-  @title = "メモ#{params[:id]}"
-  @memo_id = params[:id].to_i
-  @memos[params[:id].to_i] == {} ? (redirect '/') : (erb :show)
+  @title = 'メモ詳細'
+  @memo_index = get_index(@memos, params[:id].to_i)
+  @memo_index.nil? ? (redirect '/') : (erb :show)
 end
 
 get '/edit/:id' do
   @title = 'メモ編集'
-  @memos[params[:id].to_i] == {} ? (redirect '/') : (erb :edit)
+  @memo_index = get_index(@memos, params[:id].to_i)
+  @memo_index.nil? ? (redirect '/') : (erb :edit)
 end
 
 patch '/edit/patch/:id' do
-  @memos[params[:id].to_i]['title'] = CGI.escapeHTML(params[:title])
-  @memos[params[:id].to_i]['content'] = CGI.escapeHTML(params[:content])
-  write_file(@memos)
-  redirect '/'
+  memo_index = get_index(@memos, params[:id].to_i)
+  if memo_index.nil?
+    redirect '/'
+  else
+    @memos[memo_index]['title'] = CGI.escapeHTML(params[:title])
+    @memos[memo_index]['content'] = CGI.escapeHTML(params[:content])
+    write_file(@memos)
+    redirect '/'
+  end
 end
 
 delete '/delete/:id' do
-  @memos[params[:id].to_i] = {}
+  @memos.each do |memo|
+    @memos.delete(memo) if memo['id'] == params[:id].to_i
+  end
   write_file(@memos)
   redirect '/'
 end
